121a4130f206a2a05de244fdc1fa9500e6b3b301
feroult <feroult@gmail.com>
2016-08-23 17:08:30 -0300

    json format

--- scripts/like.sh
+++ scripts/like.sh
@@ -3,4 +3,4 @@
 POST_ID="/posts/$1"
 BACKEND_API=${LIKEDEMO_API:-http://localhost:8080/api}
 
-curl -H "Content-type: application/json" -X POST -d "{ postId: '$POST_ID' }" $BACKEND_API/likes; echo
+curl -H "Content-type: application/json" -X POST -d "{ \"postId\": \"$POST_ID\" }" $BACKEND_API/likes; echo
